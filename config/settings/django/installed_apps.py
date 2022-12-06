e3bd3830b91946c3c94513aa42036bb526cc646b
Grammy Jiang <grammy.jiang@gmail.com>
2022-12-06 23:44:27 +1100

    Split django settings module into pieces

---
+++ config/settings/django/installed_apps.py
@@ -0,0 +1,12 @@
+"""
+Application definition
+https://docs.djangoproject.com/en/4.1/ref/applications/#configuring-applications
+"""
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
---
+++ config/settings/django/middleware.py
@@ -0,0 +1,13 @@
+"""
+Middleware
+https://docs.djangoproject.com/en/4.1/topics/http/middleware/#middleware
+"""
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
