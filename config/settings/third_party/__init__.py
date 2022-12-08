8945ad8bc1a89af0cc51bf1b93832e5c1c6b5f17
Grammy Jiang <grammy.jiang@gmail.com>
2022-12-09 10:54:58 +1100

    Add django-filter

--- config/settings/third_party/__init__.py
+++ config/settings/third_party/__init__.py
@@ -2,3 +2,4 @@
 the settings of third party
 """
 from .extensions import *
+from .filter import *
---
+++ config/settings/third_party/filter.py
@@ -0,0 +1,7 @@
+"""
+django-filter
+* https://django-filter.readthedocs.io/en/stable/index.html
+"""
+from ..django import INSTALLED_APPS
+
+INSTALLED_APPS.append("django_filters")
