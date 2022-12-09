db6096ea8db079b1e5cf65794c7b6d541c053584
Grammy Jiang <grammy.jiang@gmail.com>
2022-12-09 21:35:27 +1100

    Add an empty rest folder to api

---
+++ api/rest/urls.py
@@ -0,0 +1,10 @@
+"""
+URL Dispatcher
+* https://docs.djangoproject.com/en/4.1/topics/http/urls/
+
+Router
+* https://www.django-rest-framework.org/api-guide/routers/
+"""
+from rest_framework import routers
+
+router = routers.DefaultRouter()
