520930a792f41b666ea1cca8fb54218e81062c3e
Grammy Jiang <grammy.jiang@gmail.com>
2022-12-09 21:37:10 +1100

    Add comments to config.settings

--- config/settings/third_party/rest_framework.py
+++ config/settings/third_party/rest_framework.py
@@ -1,6 +1,9 @@
 """
 Django REST framework
 * https://www.django-rest-framework.org/
+
+Django REST framework JSON:API
+* https://django-rest-framework-json-api.readthedocs.io/en/stable/index.html
 """
 from ..django import INSTALLED_APPS
 
