81b4be47dec36f7b464d99ffc4ab7728adafc541
Grammy Jiang <grammy.jiang@gmail.com>
2022-12-09 23:08:45 +1100

    Add api urls to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from api.rest.urls import router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend((path("rest-auth/", include("rest_framework.urls")),))
